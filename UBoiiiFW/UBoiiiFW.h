c72da011f6f82b8894c788cca5bd5b5cbc38b065
UBoiii <der_uboiii@outlook.com>
2022-07-05 15:37:48 +0100

    removed the UBoiii namespace

--- UBoiiiFW/UBoiiiFW.h
+++ UBoiiiFW/UBoiiiFW.h
@@ -2,20 +2,17 @@
 
 #include <iostream>
 
-namespace UBoiii
+template<typename... Arg>
+void print(Arg... args)
 {
-	template<typename... Arg>
-	void print(Arg... args)
-	{
-		([&]()
-			{
-				std::cout << args;
-			}(), ...);
-	}
+	([&]()
+		{
+			std::cout << args;
+		}(), ...);
+}
 
-	template<typename T>
-	void input(T& in)
-	{
-		std::cin >> in;
-	}
+template<typename T>
+void input(T& in)
+{
+	std::cin >> in;
 }
